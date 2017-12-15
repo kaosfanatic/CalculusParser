050b86cbb2db45abd94f07b5a9dd2dead554e800
Scott Jonker <kaosfanatic@ymail.com>
2017-12-15 12:56:53 -0600

    Helper class and placeholder form

---
+++ guiPractice/Helpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Helpers
+{
+
+
+    public static string ReverseString(string input)
+    {
+
+        char[] inputChar = input.ToCharArray();
+        string output = "";
+
+
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            output = output + inputChar[i];
+        }
+
+        return output;
+
+    }
+}
+
