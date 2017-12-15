169e1badb854cf8961ef60127386c3610e8a1241
Scott Jonker <kaosfanatic@ymail.com>
2017-12-15 12:55:46 -0600

    Setting up, but have to stop.

--- guiPractice/Form1.Designer.cs
+++ guiPractice/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace guiPractice
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTest
+            // 
+            this.lblTest.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTest.AutoSize = true;
+            this.lblTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTest.ForeColor = System.Drawing.Color.Black;
+            this.lblTest.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblTest.Location = new System.Drawing.Point(160, 9);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(80, 29);
+            this.lblTest.TabIndex = 0;
+            this.lblTest.Text = "Label";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(424, 460);
+            this.Controls.Add(this.lblTest);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.LightSlateGray;
+            this.Name = "frmMain";
+            this.Text = "Practice";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTest;
     }
 }
 
--- guiPractice/Program.cs
+++ guiPractice/Program.cs
@@ -5,18 +5,95 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace guiPractice
-{
+{ 
+
+  
     static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+
+       
         static void Main()
         {
+
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
+
+            
+
+           
+        }
+
+
+        static string MainDerive(string input)
+        {
+            string output = "0";
+
+            return output;
+        }
+
+
+        static string Parse(string input)
+        {
+            string output = "";
+            string temp = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if(input[i] == '+' || input[i] == '-')
+                {
+                    output = output + Parse(temp);
+                }
+                if(input[i] == '^')
+                {
+                    int j = i;
+
+                    string coeff = "";
+
+                    while (!Char.IsLetter(input[j]))
+                    {
+                        j--;
+                    }
+                    while (!Char.IsWhiteSpace(input[j]))
+                    {
+                        coeff = coeff + input[j];
+                    }
+
+
+                }
+
+                temp = temp + input[i];
+            }
+
+            
+
+            return output;
+        }
+
+        static string DeriveExponent(int coef, char var, int power)
+        {
+            string output = "0";
+            
+
+            return output;
+        }
+
+        static string DeriveCosine(int coef, string contained)
+        {
+            string output = "0";
+            string derContained = Parse(contained);
+
+
+
+            return output;
         }
     }
+
+   
 }
